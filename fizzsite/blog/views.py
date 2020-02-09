0c41b01c97860d3682302818ee39b2d3068f2642
Thomas Hur <33258519+hurhurhurt@users.noreply.github.com>
2020-02-08 21:04:55 -0500

    adding css/html

---
+++ fizzsite/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Fizzle',
+        'title': 'Post 1',
+        'content': 'First Post',
+        'Date_Posted': 'Feb 8th'
+    },
+    {
+        'author': 'Fizzle',
+        'title': 'Post 2',
+        'content': 'Second Post',
+        'Date_Posted': 'Feb 9th'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/login.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
