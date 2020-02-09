6c4754d9341375162a6d42e22cb2be75387147c4
Ji Hoon Park <46399486+jihnpk@users.noreply.github.com>
2020-02-08 21:41:46 -0500

    buttons

--- fizzsite/blog/urls.py
+++ fizzsite/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name ='Fizzle-Home'),
     path('about/', views.about, name ='Fizzle-About'),
+    path('login/', views.login, name ='Fizzle-Login'),
     path('register/', views.register, name = 'Fizzle-Register')
 ]
