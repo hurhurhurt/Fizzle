c212fdb751800b7ca34e18640a7365befe8820ab
Ji Hoon Park <46399486+jihnpk@users.noreply.github.com>
2020-02-08 21:54:53 -0500

    oops

--- fizzsite/fizzsite/urls.py
+++ fizzsite/fizzsite/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
-
-    #path('accounts/', include('accounts.urls')),
+    #path('', include('accounts.urls')),
     #path('accounts/', include('django.contrib.auth.urls')),
     #path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
