40724239c2af1028c3dfed362e6aee683b7d4221
Ji Hoon Park <46399486+jihnpk@users.noreply.github.com>
2020-02-09 05:26:29 -0500

    hahaha

--- fizzsite/users/models.py
+++ fizzsite/users/models.py
@@ -4,7 +4,11 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, default="Full Name")
+    location = models.CharField(max_length=50, default="Location")
+    age = models.PositiveSmallIntegerField(default=18)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(max_length=500, default="Max 500 characters.")
 
     def __str__(self):
         return f'{self.user.username} Profile'
